75e7379ac20f2661b2c105270af48243c85aaf53
beuadil <aqasimov3@std.beu.edu.az>
2024-10-25 11:06:46 +0400

    1

---
+++ src/main/kotlin/com/example/firuze/FiruzeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.firuze
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FiruzeApplication
+
+fun main(args: Array<String>) {
+    runApplication<FiruzeApplication>(*args)
+}
